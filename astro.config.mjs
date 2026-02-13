c71b6e1f44d1343eedc6d80cc81347587fe01ce0
Hugo Martínez Barros <hugo.martinez.barros@digitalizat.com>
2026-02-14 00:28:37 +0100

    feat: Implement comprehensive SEO enhancements including meta tags, JSON-LD, sitemap, and robots.txt.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cv.hugomaartinez.dev',
   adapter: cloudflare({
     imageService: 'passthrough',
     platformProxy: {
       enabled: true,
     },
   }),
-  output: 'server'
+
+  output: 'server',
+  integrations: [sitemap()]
 });
